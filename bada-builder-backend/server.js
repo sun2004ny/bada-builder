a5b03468cbca8fad6c44eecaa8ff431e420e4122
sun2004ny <sunny260604@gmail.com>
2026-01-14 22:39:01 +0530

    Revert "Update server.js"
    This reverts commit a5d6895d72e1fb69a78dc580706b822b28a88b8d.

--- bada-builder-backend/server.js
+++ bada-builder-backend/server.js
@@ -43,8 +43,6 @@ app.use(cors({
     credentials: true,
 }));
 
-app.set("trust proxy", 1);
-
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
