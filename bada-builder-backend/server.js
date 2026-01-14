a5d6895d72e1fb69a78dc580706b822b28a88b8d
sun2004ny <sunny260604@gmail.com>
2026-01-14 20:55:58 +0530

    Update server.js

--- bada-builder-backend/server.js
+++ bada-builder-backend/server.js
@@ -43,6 +43,8 @@ app.use(cors({
     credentials: true,
 }));
 
+app.set("trust proxy", 1);
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
